d417659c2879ad4b343804e8896feb804df3577c
RyxDisini <ryxvip1@gmail.com>
2024-06-07 18:50:21 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const axios = require('axios');
 const express = require('express');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
 const app = express();
 const port = process.env.PORT || 3000;
 
